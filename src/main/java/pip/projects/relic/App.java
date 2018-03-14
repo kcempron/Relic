208e2d4f212c823b6f9c27de9c66a80d55dcdaf9
Karl Cempron <cempronkarl@gmail.com>
2018-03-14 11:08:46 -0700

    Initializing Project Relic

---
+++ src/main/java/pip/projects/relic/App.java
@@ -0,0 +1,13 @@
+package pip.projects.relic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
