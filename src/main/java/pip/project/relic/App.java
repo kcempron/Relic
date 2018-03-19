05d4c7448439e73182118b99aeac145ad1e07615
Karl Cempron <cempronkarl@gmail.com>
2018-03-18 17:47:03 -0700

    Updated App and AppTest

--- src/main/java/pip/project/relic/App.java
+++ src/main/java/pip/project/relic/App.java
@@ -1,13 +1,31 @@
 package pip.project.relic;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.github.messenger4j.MessengerPlatform;
+import com.github.messenger4j.send.MessengerSendClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    /**
+     * Initializes the {@code MessengerSendClient}.
+     *
+     * @param pageAccessToken the generated {@code Page Access Token}
+     */
+    @Bean
+    public MessengerSendClient messengerSendClient(@Value("${messenger4j.pageAccessToken}") String pageAccessToken) {
+        logger.debug("Initializing MessengerSendClient - pageAccessToken: {}", pageAccessToken);
+        return MessengerPlatform.newSendClientBuilder(pageAccessToken).build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
